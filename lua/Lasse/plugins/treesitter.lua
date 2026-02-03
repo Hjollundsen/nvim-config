02186307b95d0895cf43d935ba4d5c640103a236
Lasse Hjøllund Jensen <lasse.hjollund@Gmail.com>
2026-02-03 21:36:27 +0100

    Added basic treesitter

---
+++ lua/Lasse/plugins/treesitter.lua
@@ -0,0 +1,61 @@
+return {
+    {
+        'nvim-treesitter/nvim-treesitter',
+        lazy = false,
+        build = ':TSUpdate',
+        
+        config = function()
+            -- import nvim-treesitter plugin
+            local treesitter = require("nvim-treesitter")
+
+            treesitter.setup({
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+                -- enable indentation
+                indent = { enable = true },
+
+                -- ensure these languages parsers are installed
+                ensure_installed = {
+                    "json",
+                    "javascript",
+                    "typescript",
+                    "tsx",
+                    "go",
+                    "yaml",
+                    "html",
+                    "css",
+                    "python",
+                    "http",
+                    "prisma",
+                    "markdown",
+                    "markdown_inline",
+                    "svelte",
+                    "graphql",
+                    "bash",
+                    "lua",
+                    "vim",
+                    "dockerfile",
+                    "gitignore",
+                    "query",
+                    "vimdoc",
+                    "c",
+                    "java",
+                    "rust",
+                    "ron",
+                },
+
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<C-space>",
+                        node_incremental = "<C-space>",
+                        -- scope_incremental = false,
+                        node_decremental = "<C-backspace>",
+                    },
+                },
+            })
+        end
+    },
+}
